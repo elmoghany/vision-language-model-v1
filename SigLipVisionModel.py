14b40f3bb2c0fba60172527106a76c1d80eb3368
elmoghany <m.osama.elmoghany@gmail.com>
2025-02-21 09:46:43 +0300

    transformer

--- SigLipVisionModel.py
+++ SigLipVisionModel.py
@@ -17,30 +17,33 @@ class SigLipVisionModel(nn.Module):
         self.config     = config
         
         # ---------------------------------------------------------------------
-        # 1) Patch + Position Embedding
+        # 1) Patch Embedding
         # ---------------------------------------------------------------------
         self.patch_embedding = nn.Conv2d(
-            in_channels = config.num_channels,
-            out_channels= config.dmodel,
-            kernel_size = config.patch_size,
-            stride      = config.patch_size,
+            in_channels = config.num_channels,  # 3
+            out_channels= config.dmodel,        # 768
+            kernel_size = config.patch_size,    # 16
+            stride      = config.patch_size,    # 16
             padding     = 0
         )
 
+        # e.g., (224//16) ^ 2 = 14*14 = 196 patches 
         num_patches = (config.image_size // config.patch_size) ** 2
-        
+
+        # ---------------------------------------------------------------------
+        # 2) Positional Embedding
+        # ---------------------------------------------------------------------
         # model learns an embedding for each position during training 
         # rather than computing fixed sin/cos values.
         self.positional_embedding = nn.Embedding(
-            num_embeddings  = num_patches,  # Number of patches
-            embedding_dim   = config.dmodel,# Embedding dimension
+            num_embeddings  = num_patches,  # Number of patches     # 196
+            embedding_dim   = config.dmodel,# Embedding dimension   # 768
         )
         
         # using .expand((1, -1)), you reshape this tensor to [1, num_patches]
         # instead of having 1D tensor of shape [num_patches]
         # The extra dimension (with size 1) allows the positional indices 
         # to be broadcast correctly across the batch.
-        
         # buffer => won’t be included in the model’s state dict
         self.register_buffer(
             "positional_embedding_buffer",
@@ -49,13 +52,63 @@ class SigLipVisionModel(nn.Module):
         )
 
         # ---------------------------------------------------------------------
-        # 2) Build Nx Transformer Blocks
+        # 3) Build Nx Transformer Blocks
         # ---------------------------------------------------------------------
+        # 12 layers of transformer blocks
+        self.transformer_blocks = nn.ModuleList([
+            [SigLipVisionTransformer(config) for _ in range(config.num_layers)]
+        ])
+
+        # ---------------------------------------------------------------------
+        # 4) Final Layer Norm 
+        # ---------------------------------------------------------------------
+        # Final layer norm after all transformer blocks
+        self.final_layer_norm = nn.LayerNorm(config.dmodel, eps=config.layer_norm_eps)
 
 
-
-
-    def forward(self, ):
-        # [Batch_Size, Channels, Height, Width] -> 
-        # [Batch_Size, Num_Patches, Embed_Dim]
-        pass
+    # input pixel_values
+    # [batch_size, num_channels, image_size, image_size]    => [batch_size, num_patches, dmodel]
+    # [batch_size, 3, 224, 224]                             => [batch_size, 196, 768]
+    def forward(self, pixel_values):
+        # ---------------------------------------------------------------------
+        # 1) Patch Embedding
+        # ---------------------------------------------------------------------
+        # patch embedding = conv2d
+        # [batch_size, num_channels, image_size, image_size]   
+        # [batch_size, 3, 224, 224]                             
+        patch_embeds = self.patch_embedding(pixel_values)
+        
+        # [batch_size, num_channels, image_size, image_size]   
+        # [batch_size, 3, 224, 224]                             
+        # conv2d_size = Lower((img_size + 2P - K)/S) + 1
+        # lower(224 + 2*0 - 16 / 16) + 1 = 14
+        # num_channels 3 => dmodel 768
+        # [batch_size, 768, 14, 14]
+        
+        # flatten the image size
+        # [batch_size, num_channels=dmodel, num_patches_flattened]
+        # [batch_size, 768, 196]
+        x = patch_embeds.flatten(2,-1)
+        
+        # transpose to [batch_size, num_patches_flattened, dmodel]
+        x = x.transpose(1,2)
+        
+        # ---------------------------------------------------------------------
+        # 2) Positional Embedding
+        # ---------------------------------------------------------------------
+        # add positional embedding to each patch embedding
+        x = x + self.positional_embedding(self.positional_embedding_buffer)
+        
+        # ---------------------------------------------------------------------
+        # 3) Build Nx Transformer Blocks
+        # ---------------------------------------------------------------------
+        for transformer_block in self.transformer_blocks:
+            x = transformer_block(x)
+        
+        # ---------------------------------------------------------------------
+        # 4) Final Layer Norm 
+        # ---------------------------------------------------------------------
+        x = self.final_layer_norm(x)
+        
+        # [batch_size, num_patches_flattened, dmodel]
+        return x
--- SigLipVisionTransformer.py
+++ SigLipVisionTransformer.py
@@ -12,8 +12,11 @@ class SigLipVisionTransformer(nn.Module):
         # Build 1x Transformer Block
         # ---------------------------------------------------------------------
         self.config = config
+        self.num_attention_heads = config.num_attention_heads
+        self.dropout = config.attention_drop
+        self.dmodel = config.dmodel
         #  64       = 768           //          12
-        head_dim    = config.dmodel // config.num_attention_heads
+        head_dim    = self.dmodel // self.num_attention_heads
 
         # ---------------------------------------------------------------------
         # 1) Pre-Attention LayerNorm and Q/K/V projections
@@ -28,7 +31,7 @@ class SigLipVisionTransformer(nn.Module):
         # ---------------------------------------------------------------------
         self.scale       = math.sqrt(1/config.head_dim) 
         self.dropout     = nn.Dropout(config.attention_drop) # Apply dropout to attention weights
-        self.attn_out    = nn.Linear(config.dmodel, config.dmodel)
+        self.attn_mlp    = nn.Linear(config.dmodel, config.dmodel)
         
         # ---------------------------------------------------------------------
         # 3) Post self-Attention & Pre-MLP LayerNorm2 and MLP
@@ -39,5 +42,45 @@ class SigLipVisionTransformer(nn.Module):
 
     def forward(self, transformer_input):
         
+        residual = transformer_input
+        x = self.layer_norm1(transformer_input)
+        
+        
+        # Compute Q, K, V
+        # [batch_size, seq_len, dmodel] 
+        # -> [batch_size, seq_len, num_heads, head_dim]
+        # -> [batch_size, num_heads, seq_len, head_dim]
         batch_size, seq_len, embed_dim = transformer_input
-        head_dim 
+        head_dim = embed_dim // self.num_attention_heads
+        q = self.q_proj(x).view(batch_size, seq_len, self.num_attention_heads, head_dim).transpose(1,2)
+        k = self.q_proj(x).view(batch_size, seq_len, self.num_attention_heads, head_dim).transpose(1,2)
+        v = self.q_proj(x).view(batch_size, seq_len, self.num_attention_heads, head_dim).transpose(1,2)
+        
+        # Attention scores
+        # [batch_size, num_heads, seq_len, head_dim] . [batch_size, num_heads, head_dim, seq_len]
+        # [batch_size, num_heads, seq_len, seq_len]
+        scores = torch.matmul(q, k.transpose(-2, -1)) * self.scale
+        attn_weights = torch.softmax(scores, dim=-1)
+        attn_weights = F.dropout(attn_weights, p=self.dropout, training=self.training)
+        
+        # attention output
+        # [batch_size, num_heads, seq_len, seq_len] . [batch_size, num_heads, seq_len, head_dim]
+        # [batch_size, num_heads, seq_len, head_dim]
+        attn_output = torch.matmul(attn_weights, v)
+        # reshape back to dmodel
+        # [batch_size, seq_len, num_heads, head_dim] -> [batch_size, seq_len, embed_dim]
+        attn_output = attn_output.transpose(2, 1).reshape(batch_size, seq_len, embed_dim)
+        # attn_output=attn_output.transpose(1, 2).contiguous().view(batch_size, seq_len, embed_dim)
+        attn_output = self.attn_mlp(attn_output)
+        x = residual + attn_output
+        
+        # MLP block
+        residual = x
+        x = self.layer_norm2(x)
+        x = self.mlp_fc1(x)
+        x = F.gelu(x, approximate = "tanh")
+        x = self.mlp_fc2(x)
+        x = x + residual
+        
+        #[batch_size, seq_len, embed_dim]
+        return x
