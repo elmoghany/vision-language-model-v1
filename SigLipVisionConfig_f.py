b6def778ab5afa31be839b8b2fb2d551d86e49e3
elmoghany <m.osama.elmoghany@gmail.com>
2025-02-27 22:20:55 +0300

    kv cache attention

--- SigLipVisionConfig_f.py
+++ SigLipVisionConfig_f.py
@@ -1,18 +1,18 @@
 class SigLipVisionConfig:
     def __init__(
-                self,
-                dmodel              = 768,
-                dff_inner_dim       = 3072,
-                num_layers          = 12,
-                num_attention_heads = 12,
-                num_channels        = 3,
-                image_size          = 224,
-                patch_size          = 16,
-                layer_norm_eps      = 1e-6,
-                attention_drop      = 0.0,
-                num_image_tokens: int = None,
-                **kwargs
-                ):
+        self,
+        dmodel              = 768,
+        dff_inner_dim       = 3072,
+        num_layers          = 12,
+        num_attention_heads = 12,
+        num_channels        = 3,
+        image_size          = 224,
+        patch_size          = 16,
+        layer_norm_eps      = 1e-6,
+        attention_drop      = 0.0,
+        num_image_tokens: int = None,
+        **kwargs
+        ):
         super().__init__()
         self.dmodel             = dmodel
         self.dff_inner_dim      = dff_inner_dim
