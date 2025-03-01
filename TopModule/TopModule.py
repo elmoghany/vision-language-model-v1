383a954c14900ab532a5cb94a43e32a4962f522c
elmoghany <m.osama.elmoghany@gmail.com>
2025-03-01 12:31:53 +0300

    starting in top module

---
+++ TopModule/TopModule.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+### Import Config ###
+from ..Config.VisionConfig_f import VisionConfig
+from ..Config.TextConfig_f import TextConfig
+from ..Config.FusionConfig_f import FusionConfig 
+
+### Import SigLip Vision Tower ###
+from ..SigLipVisionTower.ImageProcessor_f import ImageProcessor
+from ..SigLipVisionTower.SigLipVisionModel_f import SigLipVisionModel
+from ..SigLipVisionTower.ImageProjection_f import ImageProjection
+from ..SigLipVisionTower.ImageTextFusion_f import ImageTextFusion
+
+### Import Decoder ###
+from ..Decoder.Decoder_f import Decoder
+
+class TopModule(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
